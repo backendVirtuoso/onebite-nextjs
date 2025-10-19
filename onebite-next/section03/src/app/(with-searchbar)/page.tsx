9f5dcc2e90eccc9e05f624f7d0b492eab34755f4
backendVirtuoso <129765568+backendVirtuoso@users.noreply.github.com>
2025-10-20 00:10:24 +0900

    네비게이팅

--- onebite-next/section03/src/app/(with-searchbar)/page.tsx
+++ onebite-next/section03/src/app/(with-searchbar)/page.tsx
@@ -1,4 +1,6 @@
+import ClientComponent from "../../components/client-component";
 import styles from "./page.module.css";
+import ServerComponent from "../../components/server-component";
 
 export default function Home() {
   console.log("Home Component 실행");
@@ -6,6 +8,9 @@ export default function Home() {
   return (
     <div className={styles.page}>
       인덱스 페이지
+      <ClientComponent>
+        <ServerComponent />
+      </ClientComponent>
     </div>
   );
 }
--- onebite-next/section03/src/app/book/[id]/page.tsx
+++ onebite-next/section03/src/app/book/[id]/page.tsx
@@ -1,5 +1,14 @@
+import ClientComponent from "@/components/client-component";
+
 export default async function Page({ params }: { params: Promise<{ id: string }> }) {
   const { id } = await params;
 
-  return <div>book/[id] page입니다 : {id}</div>;
+  return (
+    <div>
+      book/[id] page입니다 : {id}
+      <ClientComponent>
+        <></>
+      </ClientComponent>
+    </div>
+  );
 }
