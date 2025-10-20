1086067a90522adc463d84fd798ecd9b42447adf
backendVirtuoso <129765568+backendVirtuoso@users.noreply.github.com>
2025-10-21 00:11:41 +0900

    App Router의 데이터 페칭

---
+++ onebite-next/section04/src/app/layout.tsx
@@ -0,0 +1,19 @@
+import "./globals.css";
+import Link from "next/link";
+import style from "./layout.module.css";
+
+export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <html lang="en">
+      <body>
+        <div className={style.container}>
+          <header>
+            <Link href={"/"}>📚 ONEBITE BOOKS</Link>
+          </header>
+          <main>{children}</main>
+          <footer>제작 @backendVirtuoso</footer>
+        </div>
+      </body>
+    </html>
+  );
+}
