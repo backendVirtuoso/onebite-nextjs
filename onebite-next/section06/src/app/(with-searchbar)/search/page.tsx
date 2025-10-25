e1b37183c32676f4e7d257d26a5a4a7f18de2424
backendVirtuoso <129765568+backendVirtuoso@users.noreply.github.com>
2025-10-25 23:12:46 +0900

    스켈레톤 UI 적용하기

--- onebite-next/section06/src/app/(with-searchbar)/search/page.tsx
+++ onebite-next/section06/src/app/(with-searchbar)/search/page.tsx
@@ -1,4 +1,5 @@
 import BookItem from "@/components/book-item";
+import BookListSkeleton from "@/components/skeleton/book-list-skeleton";
 import { BookData } from "@/types";
 import { delay } from "@/util/delay";
 import { Suspense } from "react";
@@ -29,7 +30,7 @@ export default function Page({ searchParams }: { searchParams: { q?: string } })
   return (
     <Suspense
       key={searchParams.q || ""}
-      fallback={<div>Loading...</div>}
+      fallback={<BookListSkeleton count={3} />}
     >
       <SearchResult q={searchParams.q || ""} />
     </Suspense>
