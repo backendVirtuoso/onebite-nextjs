bc7dec9291f55464cd4e2a49724bdfa3a8da3d3b
backendVirtuoso <129765568+backendVirtuoso@users.noreply.github.com>
2025-10-25 22:55:26 +0900

    스트리밍 2. 컴포넌트 스트리밍 적용하기

--- onebite-next/section06/src/app/(with-searchbar)/page.tsx
+++ onebite-next/section06/src/app/(with-searchbar)/page.tsx
@@ -1,7 +1,8 @@
 import BookItem from "@/components/book-item";
 import style from "./page.module.css";
-import books from "@/mock/books.json";
 import { BookData } from "@/types";
+import { delay } from "@/util/delay";
+import { Suspense } from "react";
 
 // export const dynamic = 'force-dynamic';
 // 특정 페이지의 유형을 강제로 static, dynaimc 페이지 설정
@@ -11,6 +12,7 @@ import { BookData } from "@/types";
 // 4. error : 페이지를 강제로 static 페이지로 설정 (설정하면 안되는 이유 -> 빌드 오류)
 
 async function AllBooks() {
+  await delay(1500);
   const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`, { cache: "force-cache" });
   if (!response.ok) {
     return <div>오류가 발생했습니다 ...</div>;
@@ -27,6 +29,7 @@ async function AllBooks() {
 }
 
 async function RecoBooks() {
+  await delay(3000);
   const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`, { next: { revalidate: 3 } });
   if (!response.ok) {
     return <div>오류가 발생했습니다 ...</div>;
@@ -42,16 +45,22 @@ async function RecoBooks() {
   );
 }
 
+export const dynamic = 'force-dynamic';
+
 export default function Home() {
   return (
     <div className={style.container}>
       <section>
         <h3>지금 추천하는 도서</h3>
-        <RecoBooks />
+        <Suspense fallback={<div>도서를 불러오는 중입니다...</div>}>
+          <RecoBooks />
+        </Suspense>
       </section>
       <section>
         <h3>등록된 모든 도서</h3>
-        <AllBooks />
+        <Suspense fallback={<div>도서를 불러오는 중입니다...</div>}>
+          <AllBooks />
+        </Suspense>
       </section>
     </div>
   );
--- onebite-next/section06/src/app/(with-searchbar)/search/page.tsx
+++ onebite-next/section06/src/app/(with-searchbar)/search/page.tsx
@@ -1,10 +1,9 @@
 import BookItem from "@/components/book-item";
 import { BookData } from "@/types";
 import { delay } from "@/util/delay";
+import { Suspense } from "react";
 
-export default async function Page({ searchParams }: { searchParams: Promise<{ q?: string }> }) {
-  const { q } = await searchParams;
-
+async function SearchResult({ q }: { q?: string }) {
   await delay(1500);
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${q}`,
@@ -23,5 +22,16 @@ export default async function Page({ searchParams }: { searchParams: Promise<{ q
         <BookItem key={book.id} {...book} />
       ))}
     </div>
+  );
+}
+
+export default function Page({ searchParams }: { searchParams: { q?: string } }) {
+  return (
+    <Suspense
+      key={searchParams.q || ""}
+      fallback={<div>Loading...</div>}
+    >
+      <SearchResult q={searchParams.q || ""} />
+    </Suspense>
   );
 }
